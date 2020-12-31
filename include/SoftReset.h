3614588714c9684f3d15f8fe667717d32145b128
Kevin Clement <kevin.clement@gmail.com>
2020-12-30 23:07:25 -0800

    remove unused class

--- include/SoftReset.h
+++ include/SoftReset.h
@@ -24,7 +24,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include <avr/wdt.h>
 
-#define soft_restart()        \
+#define soft_restart()      \
 do                          \
 {                           \
     wdt_enable(WDTO_15MS);  \
